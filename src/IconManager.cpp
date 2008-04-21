521b76f4d80c5953e8779782fe263393e57601fb
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2008-04-21 04:29:23 +0000

    Fixed looking for l10n files and hl schemes at wrong place

--- src/IconManager.cpp
+++ src/IconManager.cpp
@@ -18,7 +18,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "IconManager.h"
 
-#include <QtCore/QCoreApplication>
 #include <QtCore/QDir>
 #include <QtCore/QMap>
 #include <QtCore/QString>
--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 void checkForFirstRun() {
 	//	localizations
 	QString configPath = AppInfo::configDir();
-	QString appPath = QCoreApplication::applicationDirPath();
+	QString appPath = AppInfo::appDirPath();
 
 	QDir configDir(configPath);
 	if (!configDir.exists()) {

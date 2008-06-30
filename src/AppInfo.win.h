63fac04821a9954ed38b1b305c0fd12c03a67c50
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2008-06-30 22:10:57 +0000

    Added copyright header to AppInfo.win.h

--- src/AppInfo.win.h
+++ src/AppInfo.win.h
@@ -1,3 +1,21 @@
+/*
+JuffEd - A simple text editor
+Copyright 2007-2008 Mikhail Murzin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 #ifndef _APP_INFO_H_
 #define _APP_INFO_H_
 

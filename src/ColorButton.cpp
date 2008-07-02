8e75cde437b3e041ea9f092a47eb6e4757799f51
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2008-07-02 21:53:28 +0000

    Added missed files for previous commit

---
+++ src/ColorButton.cpp
@@ -0,0 +1,43 @@
+#include "ColorButton.h"
+
+#include <QtGui/QColorDialog>
+#include <QtGui/QPainter>
+#include <QtGui/QPixmap>
+#include <QtGui/QPushButton>
+
+#include "Log.h"
+
+ColorButton::ColorButton(QPushButton* btn, const QColor& color) : QObject(btn) {
+	btn_ = btn;
+	
+	connect(btn, SIGNAL(clicked()), this, SLOT(clicked()));
+	setBtnColor(color);
+}
+
+ColorButton::~ColorButton() {
+	Log::print("Color btn destroyed");
+}
+
+void ColorButton::clicked() {
+	QColor c = QColorDialog::getColor(color_);
+	if (c.isValid()) {
+		setBtnColor(c);
+	}
+}
+
+void ColorButton::setBtnColor(const QColor& color) {
+	if (btn_ == 0)
+		return;
+
+	color_ = color;
+
+	Log::print(QString("%1 %2").arg(btn_->width()).arg(btn_->height()));
+	QPixmap pm(btn_->width() - 10, btn_->height() - 10);
+	QPainter p(&pm);
+	p.setBrush(color);
+	p.setPen(QPen(color));
+	p.drawRect(0, 0, pm.width(), pm.height());
+	btn_->setIconSize(pm.size());
+	btn_->setIcon(QIcon(pm));
+}
+
---
+++ src/ColorButton.h
@@ -0,0 +1,29 @@
+#ifndef _COLOR_BUTTON_H_
+#define _COLOR_BUTTON_H_
+
+#include <QtCore/QObject>
+#include <QtGui/QColor>
+
+class QPushButton;
+	
+class ColorButton : public QObject {
+Q_OBJECT
+public:
+	ColorButton(QPushButton*, const QColor& color);
+	virtual ~ColorButton();
+
+	QColor color() const {
+		return color_;
+	}
+
+public slots:
+	void clicked();
+
+private:
+	void setBtnColor(const QColor&);
+
+	QColor color_;
+	QPushButton* btn_;
+};
+
+#endif

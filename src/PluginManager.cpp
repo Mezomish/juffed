d766857c9aee57de42019217ab50fdec8562241d
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2008-08-07 13:45:52 +0000

    Fixed compiling issue on Windows

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -21,7 +21,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <QtCore/QDir>
 #include <QtCore/QPluginLoader>
 
+#ifdef Q_OS_WIN
+#include "AppInfo.win.h"
+#else
 #include "AppInfo.h"
+#endif
+
 #include "Log.h"
 #include "PluginInterface.h"
 #include "PluginSettings.h"

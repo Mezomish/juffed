b48222bece7f073d245dae4f9bd6b7a50bf8cd9e
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2009-03-09 20:10:18 +0000

    Fixed: missing includes

--- src/gui/CharsetsSettingsPage.cpp
+++ src/gui/CharsetsSettingsPage.cpp
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "CharsetsSettingsPage.h"
 
 #include "CharsetsSettings.h"
+#include "Log.h"
 
 CharsetsSettingsPage::CharsetsSettingsPage(QWidget* parent) : QWidget(parent) {
 	JUFFENTRY;

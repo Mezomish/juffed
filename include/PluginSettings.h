e5ad83956c59d5d22c898ca5a4c2f2fc165aec0a
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2010-04-04 23:34:50 +0000

    Added plugins settings

---
+++ include/PluginSettings.h
@@ -0,0 +1,80 @@
+/*
+JuffEd - An advanced text editor
+Copyright 2007-2010 Mikhail Murzin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef __JUFFED_PLUGINS_SETTINGS_H__
+#define __JUFFED_PLUGINS_SETTINGS_H__
+
+class JuffPlugin;
+
+#include "Settings.h"
+
+class PluginSettings : public Settings {
+public:
+	/** Returns if plugin with name \a pluginName is
+	 *  enabled (supposed to be loaded).
+	 */
+	static bool pluginEnabled(const QString& pluginName);
+
+	/**
+	 */
+	static void setPluginEnabled(const QString& pluginName, bool enabled);
+
+	/**
+	 * set()
+	 *
+	 * Stores a string \param value with a key \param key for \param plugin.
+	 */
+	static void set(const JuffPlugin* plugin, const QString& key, const QString& value);
+	
+	/**
+	 * set()
+	 *
+	 * Stores a bool \param value with a key \param key for \param plugin.
+	 */
+	static void set(const JuffPlugin* plugin, const QString& key, bool value);
+	
+	/**
+	 * set()
+	 *
+	 * Stores an integer \param value with a key \param key for \param plugin.
+	 */
+	static void set(const JuffPlugin* plugin, const QString& key, int value);
+	
+	/**
+	 * getString()
+	 *
+	 * Returns a string value with a key \param key for \param plugin.
+	 */
+	static QString getString(const JuffPlugin* plugin, const QString& key);
+
+	/**
+	 * getBool()
+	 *
+	 * Returns a bool value with a key \param key for \param plugin.
+	 */
+	static bool getBool(const JuffPlugin* plugin, const QString& key);
+
+	/**
+	 * getInt()
+	 *
+	 * Returns an integer value with a key \param key for \param plugin.
+	 */
+	static int getInt(const JuffPlugin* plugin, const QString& key);
+};
+
+#endif // __JUFFED_PLUGINS_SETTINGS_H__
---
+++ src/lib/PluginSettings.cpp
@@ -0,0 +1,65 @@
+/*
+JuffEd - An advanced text editor
+Copyright 2007-2010 Mikhail Murzin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "PluginSettings.h"
+
+#include "JuffPlugin.h"
+
+bool PluginSettings::pluginEnabled(const QString& pluginName) {
+	return Settings::boolValue("Plugins", pluginName);
+}
+
+void PluginSettings::setPluginEnabled(const QString& pluginName, bool enabled) {
+	Settings::setValue("Plugins", pluginName, enabled);
+}
+
+void PluginSettings::set(const JuffPlugin* plugin, const QString& key, const QString& value) {
+	if ( plugin )
+		Settings::setValue(plugin->name(), key, value);
+}
+
+void PluginSettings::set(const JuffPlugin* plugin, const QString& key, bool value) {
+	if ( plugin )
+		Settings::setValue(plugin->name(), key, value);
+}
+
+void PluginSettings::set(const JuffPlugin* plugin, const QString& key, int value) {
+	if ( plugin )
+		Settings::setValue(plugin->name(), key, value);
+}
+
+QString PluginSettings::getString(const JuffPlugin* plugin, const QString& key) {
+	if ( plugin )
+		return Settings::stringValue(plugin->name(), key);
+	else
+		return "";
+}
+
+bool PluginSettings::getBool(const JuffPlugin* plugin, const QString& key) {
+	if ( plugin )
+		return Settings::boolValue(plugin->name(), key);
+	else
+		return false;
+}
+
+int PluginSettings::getInt(const JuffPlugin* plugin, const QString& key) {
+	if ( plugin )
+		return Settings::intValue(plugin->name(), key);
+	else
+		return -1;
+}

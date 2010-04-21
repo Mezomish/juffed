a4452557f021536bd3259e548792646cedcca8cd
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2010-04-21 03:09:55 +0000

    Removed unneded include

--- include/JuffPlugin.h
+++ include/JuffPlugin.h
@@ -22,7 +22,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "DocHandlerInt.h"
 #include "Enums.h"
 #include "JuffAPI.h"
-#include "PluginNotifier.h"
 #include "Types.h"
 
 #include <QtGui/QWidgetList>

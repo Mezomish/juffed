cd21a7cdc7dba07c1fcd28cdb66a631b11a6bf01
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2010-11-22 20:43:37 +0000

    Fixed: correct undo-ing of duplicated text

--- src/app/qsci/SciDoc.cpp
+++ src/app/qsci/SciDoc.cpp
@@ -856,14 +856,17 @@ void SciDoc::toggleCommentBlock() {
 void SciDoc::duplicateText() {
 	if ( int_->curEdit_ == NULL ) return;
 	
+	int_->curEdit_->beginUndoAction();
 	if ( int_->curEdit_->hasSelectedText() ) {
 		int col1, row1, col2, row2;
 		getSelection(col1, row1, col2, row2);
 		int_->curEdit_->SendScintilla(QsciScintilla::SCI_SELECTIONDUPLICATE);
 		setSelection(col1, row1, col2, row2);
 	}
-	else
+	else {
 		int_->curEdit_->SendScintilla(QsciScintilla::SCI_LINEDUPLICATE);
+	}
+	int_->curEdit_->endUndoAction();
 }
 
 void SciDoc::unindent() {

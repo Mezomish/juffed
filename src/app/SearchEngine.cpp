5d62965ba30f66223b6ba5a57554e2ddcd689e77
mezomish <mezomish@cab69d93-124a-0410-a35b-bdf591ef4405>
2010-11-27 04:09:30 +0000

    Fixed: includes

--- src/app/SearchEngine.cpp
+++ src/app/SearchEngine.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "SearchEngine.h"
 
+#include "Constants.h"
 #include "Document.h"
 #include "JuffMW.h"
 #include "Log.h"
--- src/app/qsci/SciDoc.cpp
+++ src/app/qsci/SciDoc.cpp
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "AppInfo.h"
 #include "AutocompleteSettings.h"
+#include "Constants.h"
 #include "JuffScintilla.h"
 #include "Log.h"
 #include "EditorSettings.h"
